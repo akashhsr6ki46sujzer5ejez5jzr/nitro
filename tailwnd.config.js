deb51741245333bce5f83510fd3a2b8b59313dab
akash <sbharatha399@gmail.com>
2025-01-12 19:41:08 +0530

    first commit

---
+++ tailwnd.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          roboto: ['Syne', 'sans-serif'], // Custom font ka naam 'Roboto' define kiya
+        },
+      },
+    },
+    plugins: [],
+  }
+  
